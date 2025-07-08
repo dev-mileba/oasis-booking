8c5b33645dd9cfe2ae9db2c2caa6f62776844c18
dev-mileba <developer.mileba@gmail.com>
2025-07-08 17:26:25 +0200

    setup with styled component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,26 @@
+import styled from 'styled-components';
+import GlobalStyles from './styles/GlobalStyles';
+import Button from './ui/Button';
+import Input from './ui/Input';
+import Heading from './ui/Heading';
+
+const StyledApp = styled.div`
+	background-color: red;
+	padding: 20px;
+`;
+
 function App() {
-	return <div>Hello World</div>;
+	return (
+		<>
+			<GlobalStyles />
+			<StyledApp>
+				<Heading>The wild oasis</Heading>
+				<Button onClick={() => alert('check in')}>Check in</Button>
+				<Button onClick={() => alert('check out')}>Check out</Button>
+				<Input type="number" placeholder="Number of guest"></Input>
+			</StyledApp>
+		</>
+	);
 }
 
 export default App;
--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -1,14 +1,9 @@
+import { createGlobalStyle } from 'styled-components';
+// create global styles
+// Step 1
+const GlobalStyles = createGlobalStyle`
 /*
-<link rel="preconnect" href="https://fonts.googleapis.com" />
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-<link
-  href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap"
-  rel="stylesheet"
-/>
-<link
-  href="https://fonts.googleapis.com/css2?family=Sono:wght@400;500;600&display=swap"
-  rel="stylesheet"
-/>
+
 */
 
 /* Colors adapted from https://tailwindcss.com/docs/customizing-colors */
@@ -194,3 +189,6 @@ FOR DARK MODE
 --image-grayscale: 10%;
 --image-opacity: 90%;
 */
+
+`;
+export default GlobalStyles;
