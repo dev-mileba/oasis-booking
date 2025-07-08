05b47cbdf1fe1f7ccda1984177e8bb5c0f7a0b80
dev-mileba <developer.mileba@gmail.com>
2025-07-08 19:17:31 +0200

    wip

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,10 @@ import GlobalStyles from './styles/GlobalStyles';
 import Button from './ui/Button';
 import Input from './ui/Input';
 import Heading from './ui/Heading';
+import Row from './ui/Row';
 
 const StyledApp = styled.div`
-	background-color: red;
+	/* background-color: red; */
 	padding: 20px;
 `;
 
@@ -14,10 +15,29 @@ function App() {
 		<>
 			<GlobalStyles />
 			<StyledApp>
-				<Heading>The wild oasis</Heading>
-				<Button onClick={() => alert('check in')}>Check in</Button>
-				<Button onClick={() => alert('check out')}>Check out</Button>
-				<Input type="number" placeholder="Number of guest"></Input>
+				<Row>
+					<Row type="horizontal">
+						<Heading as="h1">The wild oasis</Heading>
+						<div>
+							<Heading as="h2">Check in and out</Heading>
+							<Button onClick={() => alert('check in')}>Check in</Button>
+							<Button
+								variation="secondary"
+								size="small"
+								onClick={() => alert('check out')}
+							>
+								Check out
+							</Button>
+						</div>
+					</Row>
+					<Row>
+						<Heading as="h3">Form</Heading>
+						<form>
+							<Input type="number" placeholder="Number of guest"></Input>
+							<Input type="number" placeholder="Number of guest"></Input>
+						</form>
+					</Row>
+				</Row>
 			</StyledApp>
 		</>
 	);
