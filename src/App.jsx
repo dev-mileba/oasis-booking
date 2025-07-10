c6869b6f622911be98831bb7ee620644281f4028
dev-mileba <developer.mileba@gmail.com>
2025-07-10 16:24:53 +0200

    added react toast

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import GlobalStyles from './styles/GlobalStyles';
 import AppLayout from './ui/AppLayout';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { Toaster } from 'react-hot-toast';
 
 // Setting up React Query
 const queryClient = new QueryClient({
@@ -25,6 +26,7 @@ function App() {
 	return (
 		<QueryClientProvider client={queryClient}>
 			<ReactQueryDevtools initialIsOpen={false} />
+
 			<GlobalStyles />
 			<BrowserRouter>
 				<Routes>
@@ -41,6 +43,26 @@ function App() {
 					<Route path="*" element={<PageNotFound />} />
 				</Routes>
 			</BrowserRouter>
+			<Toaster
+				position="top-center"
+				gutter={12}
+				containerStyle={{ margin: '8px' }}
+				toastOptions={{
+					success: {
+						duration: 3000,
+					},
+					error: {
+						duration: 5000,
+					},
+					style: {
+						fontSize: 'i6px',
+						maxWidth: '500px',
+						padding: '16px 24px',
+						backgroundColor: 'var(--color-grey-0)',
+						color: 'var(--color-grey-700)',
+					},
+				}}
+			/>
 		</QueryClientProvider>
 	);
 }
